355b1717aa7d9f1e85a59c33599be4a598c44aa2
dummy8921 <83942230+dummy8921@users.noreply.github.com>
2021-10-27 19:51:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var random_number;
+timer_counter=0;
+timer_check="";
+drawn_sketch="";
+answer_holder="";
+score=0;
+function setup()
+{
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+function clearCanvas()
+{
+    background("white");
+}
